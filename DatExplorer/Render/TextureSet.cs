d7ef22fecd542ed666ba0f28d31b0b6f24b3cce6
gmriggs <gmriggs@gmail.com>
2018-12-10 13:01:07 -0500

    Updating EnvCell instancing (#7)

--- DatExplorer/Render/TextureSet.cs
+++ DatExplorer/Render/TextureSet.cs
@@ -5,15 +5,20 @@ namespace DatExplorer.Render
 {
     public class TextureSet: IEquatable<TextureSet>
     {
+        public uint Environment;
         public List<uint> SurfaceIDs;
 
         public TextureSet(R_EnvCell envCell)
         {
+            Environment = envCell.EnvCell.EnvironmentID;
             SurfaceIDs = envCell.EnvCell._envCell.Surfaces;
         }
 
         public bool Equals(TextureSet textureSet)
         {
+            if (Environment != textureSet.Environment)
+                return false;
+
             if (SurfaceIDs.Count != textureSet.SurfaceIDs.Count)
                 return false;
 
@@ -28,6 +33,8 @@ namespace DatExplorer.Render
         public override int GetHashCode()
         {
             int hash = 0;
+
+            hash = (hash * 397) ^ Environment.GetHashCode();
 
             foreach (var surfaceID in SurfaceIDs)
                 hash = (hash * 397) ^ surfaceID.GetHashCode();
