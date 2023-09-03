37031ddf2ac6a52fcfe1ddf2db199b607644eb61
gmriggs <gmriggs@gmail.com>
2023-09-03 09:54:58 -0400

    generate build timestamp at compile time for about box (#75)

--- ACViewer/View/About.xaml.cs
+++ ACViewer/View/About.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
 
 namespace ACViewer.View
 {
@@ -7,14 +10,39 @@ namespace ACViewer.View
     /// </summary>
     public partial class About : Window
     {
+        public string RunText => "ACViewer - build " + GetBuildDate(Assembly.GetExecutingAssembly()).ToString("yyyy.MM.dd");
+       
         public About()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)
         {
             Close();   
+        }
+
+        // https://www.meziantou.net/getting-the-date-of-build-of-a-dotnet-assembly-at-runtime.htm
+        private static DateTime GetBuildDate(Assembly assembly)
+        {
+            const string BuildVersionMetadataPrefix = "+build";
+
+            var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            if (attribute?.InformationalVersion != null)
+            {
+                var value = attribute.InformationalVersion;
+                var index = value.IndexOf(BuildVersionMetadataPrefix);
+                if (index > 0)
+                {
+                    value = value.Substring(index + BuildVersionMetadataPrefix.Length);
+                    if (DateTime.TryParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var result))
+                    {
+                        return result;
+                    }
+                }
+            }
+            return default;
         }
     }
 }
