9d5b6e7c918eb3a851907646fe3e1af26b8750b4
Ty Conner <tyconner@itcproductions.com>
2025-11-20 00:33:42 -0500

    Update AppVeyorInstall.ps1

--- AppVeyor/AppVeyorInstall.ps1
+++ AppVeyor/AppVeyorInstall.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -UseBasicParsing -OutFile "$env:temp\dotnet-install.ps1"
-& $env:temp\dotnet-install.ps1 -Architecture x64 -Version '3.1' -InstallDir "$env:ProgramFiles\dotnet"
+& $env:temp\dotnet-install.ps1 -Architecture x64 -Version '3.1.32' -InstallDir "$env:ProgramFiles\dotnet"
